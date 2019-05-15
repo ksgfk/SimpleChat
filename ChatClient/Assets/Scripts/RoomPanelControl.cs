ac74d40ceaaece2b6a7eb9fe1e118f1f46033e40
ksgfk <37179748+ksgfk@users.noreply.github.com>
2019-05-15 17:19:52 +0800

    简易Unity客户端

---
+++ ChatClient/Assets/Scripts/RoomPanelControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomPanelControl : MonoBehaviour
+{
+	public InputField chatInput;
+	public Text chatText;
+	public ScrollRect scrollRect;
+	public string username;
+
+	
+	public void OnSendButtonClick()
+	{
+		if (chatInput.text != string.Empty)
+		{
+			string addText = "\n  " + "<color=red>" + username + "</color>: " + chatInput.text;
+			chatText.text += addText;
+			chatInput.text = "";
+			chatInput.ActivateInputField();
+			Canvas.ForceUpdateCanvases();
+			scrollRect.verticalNormalizedPosition = 0f;
+			Canvas.ForceUpdateCanvases();
+		}
+	}
+}
